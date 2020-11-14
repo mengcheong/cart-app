685de5cd763c1e8294a219f205c1377ebbabb88a
mengcheong <mengcheong@gmail.com>
2020-11-15 02:53:47 +0800

    Shared

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import e from "express";
+import express from 'express';
+import data from "./data.js";
+
+const app = express();
+
+app.get('/api/products',(req,res) => {
+    //res.send('server is ready');
+    res.status(200).send(data.products);
+});
+
+app.get('/',(req,res) => {
+    //res.send('server is ready');
+    res.status(200).send('server is ready');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
