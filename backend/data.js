801576b148efef4edab667dbf6c3668616494876
mengcheong <mengcheong@gmail.com>
2020-11-21 01:59:20 +0800

    Shared

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+  users: [
+    {
+      name:'lmc',
+      email:'mengcheong@gmail.com',
+      password: bcrypt.hashSync('1234',8),
+      isAdmin:true
+    },
+    {
+      name:'john',
+      email:'john@example.com',
+      password: bcrypt.hashSync('1234',8),
+      isAdmin:false
+    }
+  ],
   products: [
     {
       _id: '1',
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 //import e from "express";
 import express from 'express';
+import mongoose from 'mongoose';
 import data from "./data.js";
+import userRouter from './routers/userRouter.js';
 
+
+//uYzCZHgWQxrGlGZI
+//mongodb+srv://admin:<password>@cluster0.0adni.mongodb.net/<dbname>?retryWrites=true&w=majority
+const connection_url = 'mongodb+srv://admin:uYzCZHgWQxrGlGZI@cluster0.0adni.mongodb.net/cartdb?retryWrites=true&w=majority';
 const app = express();
+
+mongoose.connect(connection_url,{
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  useUnifiedTopology:true,
+});
+
+app.use('/api/users',userRouter);
 
 app.get('/api/products',(req,res) => {
     //res.send('server is ready');
@@ -18,13 +32,16 @@ app.get('/api/products/:id',(req,res) => {
   }
   else{
     res.status(404).send({message: 'Product not found'});
-  }
-   
+  }   
 });
 
 app.get('/',(req,res) => {
     //res.send('server is ready');
     res.status(200).send('server is ready');
+});
+
+app.use((err,req,res,next) => {
+  res.status(500).send({message: err.message});
 });
 
 const port = process.env.PORT || 5000;
